790a6ec402fd6f0a9d00ba48011baaaf0f8072d0
Luis <delapenl@gmail.com>
2015-09-12 12:02:04 -0600

    updated pets.js to connect to db

---
+++ server/models/pets.js
@@ -0,0 +1,21 @@
+// required mongoose, Schema and Schema object in variable.
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Pet = new Schema(
+  {
+    name: String,
+    Type: String,
+    Age: Number
+  }
+);
+
+// ask what this does?
+process.env.DB_HOST = 'mongodb://localhost/pets';
+
+// connect to mongoose or database?
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/pets');
+
+// export Schema so that it is usable in other files.
+module.exports = mongoose.model('pets', Pet);
+
+
